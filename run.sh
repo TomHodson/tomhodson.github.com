8c841451eacaae6abef527de01d2cd6200612522
Tom <thomas.hodson@ecmwf.int>
2024-07-29 11:13:43 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+bundle install
 echo Open http://0.0.0.0:4000
 # bundle exec jekyll serve --draft --future --live --incremental
-bundle exec jekyll serve --live --incremental --livereload
+bundle exec jekyll serve --live --incremental --livereload --future --host 0.0.0.0
