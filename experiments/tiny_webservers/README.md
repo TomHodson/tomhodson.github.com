df53516e4542c1fc63739caface1d07d7be47d0e
Tom Hodson <thomas.c.hodson@gmail.com>
2025-07-29 11:26:40 +0200

    Update tiny webserver

---
+++ experiments/tiny_webservers/README.md
@@ -0,0 +1,5 @@
+# Tiny Webservers
+
+## Single Threaded
+`cargo build --release --bin single_threaded`
+`./target/release/single_threaded localhost:4536`
--- experiments/tiny_webservers/src/bin/single_threaded.rs
+++ experiments/tiny_webservers/src/bin/single_threaded.rs
@@ -30,19 +30,22 @@ fn handle_connection(stream: &mut TcpStream, hitcount: &mut usize) -> io::Result
     stream.read(&mut buf).expect("Failed to read from socket.");
     let request = str::from_utf8(&buf).expect("Request is not valid utf8");
 
-    println!("Got request: {}", request.split("\r\n").nth(0).unwrap_or(&request));
+    print!("{}", request.split("\r\n").nth(0).unwrap_or(&request));
 
-    let (status, content) = if request.starts_with("GET / HTTP/1.1\r\n") {
+    let expected_request = "GET /tiny_servers/single_threaded HTTP/1.1\r\n";
+    let (status, content) = if request.starts_with(expected_request) {
         *hitcount = (*hitcount).saturating_add(1);
         ("200 OK", format!("{{\"hits\": {}}}\n", hitcount))
     } else {
         ("404 NOT FOUND", "404 not found".to_string())
     };
+    println!(" - {}", status);
 
     let length = content.len();
     let response = format!(
         "HTTP/1.1 {status}\r\n\
                                     Content-Type: application/json\r\n\
+                                    Access-Control-Allow-Origin: *\r\n\
                                     Content-Length: {length}
                                     \r\n\r\n\
                                     {content}"
